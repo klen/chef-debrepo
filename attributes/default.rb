27e3687d87ab9b30e29302f92158787c9563b6ee
Kirill Klenov <horneds@gmail.com>
2013-03-01 16:00:13 +0800

    Change default dir.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['debrepo']['host'] = nil
 default['debrepo']['name'] = "main"
-default['debrepo']['source_dir'] = "/lib/debrepo"
+default['debrepo']['source_dir'] = "/usr/lib/debrepo"
 default['debrepo']['nginx_proxy'] = true
